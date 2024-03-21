cb9ea6940d964c453a2d393dd6f022fef7513871
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-03-21 17:20:20 +0800

    stuck

--- main.c
+++ main.c
@@ -10,9 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdbool.h>
 #include <stdio.h>
+
+bool	is_blank(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+bool 	is_pipe(char c)
+{
+	return (c == '|');
+}
+
+bool	is_redirect(char c)
+{
+	return (c == '<' || c == '>');
+}
+
+bool	is_meta(char c)
+{
+	return (is_pipe(c) || is_redirect(c) || is_blank(c));
+}
+
+void	parser(char *line)
+{
+	int		i;
+	char	c;
+
+	if (!line)
+		return ;
+	// run through the line
+	// look for operators?
+	// in order of precendence?
+	
+}
+
 
 int main(void)
 {
-	printf("hello\n");
+	char *line = NULL;
+
+	while (true)
+	{
+		if (line)
+			free(line);
+		 line = get_next_line(0);
+		 parser(line);
+	}
 }
