891c6bd8074a23dbdf693a54dc2d86813f980bb3
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-03-26 17:12:03 +0800

    added cwd to paths

--- main.c
+++ main.c
@@ -26,100 +26,8 @@ char	**find_paths(char **envp);
 void	free_str(char **p);
 char	*check_valid_cmd(char **paths, char *cmd);
 void	run_child_command(char **paths, char **cmd);
+void	parse_quotes(char *line, char ***words);
 
-
-
-
-void	add_to_words(char ***words, char *str)
-{
-	int		num_strs;
-	size_t	size;
-
-	num_strs = 0;
-	while (*words && (*words)[num_strs] != NULL)
-		num_strs++;
-	size = (num_strs + 1) * sizeof(char *);
-	*words = (char **) ft_realloc(*words, size, size + sizeof(char *));
-	(*words)[num_strs] = str;
-	(*words)[num_strs + 1] = NULL;
-}
-
-
-void	split_line_by_space(char ***words, char *str)
-{
-	int		x;
-	char	**line;
-
-	x = 0;
-	line = ft_split(str, ' ');
-	while (line[x])
-		add_to_words(words, line[x++]);
-	free(line);
-}
-
-int	process_subquote(char *arg, char ***words, char *qstart, char qchar)
-{
-	int		qlen;
-	char	*cmd;
-	char	*qarg;
-
-	if (qstart != arg)
-	{
-		cmd = ft_substr(arg, 0, qstart - arg);
-		split_line_by_space(words, cmd);
-		free(cmd);
-	}
-	qlen = 0;
-	while (qstart[qlen + 1] && qstart[qlen + 1] != qchar)
-		qlen++;
-	if (qlen)
-	{
-		qarg = ft_substr(qstart, 1, qlen + 1);
-		add_to_words(words, qarg);
-		qarg[qlen] = 0;
-	}
-	return (qlen);
-}
-
-
-void parse_quotes(char *line, char ***words)
-{
-	// take whole line
-	// split into "words"
-	// if opening quote
-	// look for matching ending quote
-	// everything in quotes will be one word
-	char	*qstart;
-	char	qchar;
-	int		j;
-	int		qlen;
-
-	j = -1;
-	qchar = 0;
-	qstart = NULL;
-	while (line && line[++j])
-	{
-		if (line[j] == '\'' || line[j] == '"')
-		{
-			qstart = line + j;
-			qchar = line[j];
-			break ;
-		}
-	}
-	if (qstart)
-	{
-		qlen = process_subquote(line, words, qstart, qchar);
-		if (j + qlen < ft_strlen(line))
-		{
-			if (line[j + qlen + 1] && (line[j + qlen + 1] == qchar))
-				j++;
-			j++;
-			parse_quotes(line + j + qlen, words);
-		}
-	}
-	else
-		split_line_by_space(words, line);
-}
 
 int main(int ac, char **av, char **envp)
 {
@@ -185,6 +93,92 @@ int main(int ac, char **av, char **envp)
 	free(paths);
 }
 
+void	add_to_words(char ***words, char *str)
+{
+	int		num_strs;
+	size_t	size;
+
+	num_strs = 0;
+	while (*words && (*words)[num_strs] != NULL)
+		num_strs++;
+	size = (num_strs + 1) * sizeof(char *);
+	*words = (char **) ft_realloc(*words, size, size + sizeof(char *));
+	(*words)[num_strs] = str;
+	(*words)[num_strs + 1] = NULL;
+}
+
+
+void	split_line_by_space(char ***words, char *str)
+{
+	int		x;
+	char	**line;
+
+	x = 0;
+	line = ft_split(str, ' ');
+	while (line[x])
+		add_to_words(words, line[x++]);
+	free(line);
+}
+
+int	process_subquote(char *arg, char ***words, char *qstart, char qchar)
+{
+	int		qlen;
+	char	*cmd;
+	char	*qarg;
+
+	if (qstart != arg)
+	{
+		cmd = ft_substr(arg, 0, qstart - arg);
+		split_line_by_space(words, cmd);
+		free(cmd);
+	}
+	qlen = 0;
+	while (qstart[qlen + 1] && qstart[qlen + 1] != qchar)
+		qlen++;
+	if (qlen)
+	{
+		qarg = ft_substr(qstart, 1, qlen + 1);
+		add_to_words(words, qarg);
+		qarg[qlen] = 0;
+	}
+	return (qlen);
+}
+
+
+void parse_quotes(char *line, char ***words)
+{
+	char	*qstart;
+	char	qchar;
+	int		j;
+	int		qlen;
+
+	j = -1;
+	qchar = 0;
+	qstart = NULL;
+	while (line && line[++j])
+	{
+		if (line[j] == '\'' || line[j] == '"')
+		{
+			qstart = line + j;
+			qchar = line[j];
+			break ;
+		}
+	}
+	if (qstart)
+	{
+		qlen = process_subquote(line, words, qstart, qchar);
+		if (j + qlen < ft_strlen(line))
+		{
+			if (line[j + qlen + 1] && (line[j + qlen + 1] == qchar))
+				j++;
+			j++;
+			parse_quotes(line + j + qlen, words);
+		}
+	}
+	else
+		split_line_by_space(words, line);
+}
+
 
 void free_str(char **str)
 {
@@ -205,6 +199,13 @@ char **find_paths(char **envp)
 		paths = ft_split(*envp + 5, ':');
 	else
 		paths = (char **)(ft_calloc(1, sizeof(char *)));
+
+	// calc num of paths inorder to realloc first one to cwd
+	int i = 0;
+	while (paths[i])
+		i++;
+	paths = ft_realloc(paths, i * sizeof(char *), (i + 1) * sizeof(char *));
+	paths[i] = getcwd(NULL, 500);
 	return paths;
 }
 
