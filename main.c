276bdf080048240da0fe9e7d57f3c2d7c693cce7
Tinaesh Jegadessan <tjegades@student.42singapore.sg>
2024-03-26 13:23:58 +0800

    suppress readline errors

--- main.c
+++ main.c
@@ -11,52 +11,61 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <stdbool.h>
-#include <stdio.h>
+// #include <stdio.h>
+#include <unistd.h> // write, getcwd
+#include <stdlib.h> // malloc, free
 
-bool	is_blank(char c)
+void free_str(char *p)
 {
-	return (c == ' ' || c == '\t' || c == '\n');
-}
-
-bool 	is_pipe(char c)
-{
-	return (c == '|');
-}
-
-bool	is_redirect(char c)
-{
-	return (c == '<' || c == '>');
-}
-
-bool	is_meta(char c)
-{
-	return (is_pipe(c) || is_redirect(c) || is_blank(c));
-}
-
-void	parser(char *line)
-{
-	int		i;
-	char	c;
-
-	if (!line)
-		return ;
-	// run through the line
-	// look for operators?
-	// in order of precendence?
-	
-}
-
-
-int main(void)
-{
-	char *line = NULL;
-
-	while (true)
+	if (p)
 	{
-		if (line)
-			free(line);
-		 line = get_next_line(0);
-		 parser(line);
+		free(p);
+		p = NULL;
 	}
+}
+
+char **find_paths(char **envp)
+{
+	char	**paths;
+
+	while (*envp && ft_strncmp(*envp, "PATH", 4))
+		envp++;
+	if (*envp)
+		paths = ft_split(*envp + 5, ':');
+	else
+		paths = (char **)(ft_calloc(1, sizeof(char *)));
+	return paths;
+}
+
+int main(int ac, char **av, char **envp)
+{
+	char	*line;
+	char	**paths;
+
+
+	int i = 1;
+
+	// paths = find_paths(envp);
+
+	// while (paths[i])
+	// 	printf("%s\n", paths[i++]);
+
+	line = NULL;
+	while (i--)
+	{
+		free_str(line);
+		line = readline("$> ");
+		if (line && *line)
+			add_history(line);
+	}
+	free_str(line);
+
+	// while (paths)
+	// {
+	// 	free_str(*paths);
+	// 	paths++;
+	// }
 }
